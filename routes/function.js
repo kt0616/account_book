5ed79cdc85106a5a7e65503b5c846368bdf6501e
test <test>
2016-05-09 23:38:04 +0900

    test

--- routes/function.js
+++ routes/function.js
@@ -1,3 +1,7 @@
+var crypto = require("crypto");
 exports.separateNum = function(num){
     return String(num).replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+}
+exports.getHashValue = function(str){
+  
 }
